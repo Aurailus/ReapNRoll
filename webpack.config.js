821245c5b5c3a7ee0da0fbd5e14049183b9c2578
Aurailus <me@auri.xyz>
2023-01-06 11:11:04 -0800

    Project Build Tools & Setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+// const CopyPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/Main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+	plugins: [
+		new HTMLWebpackPlugin()
+	]
+};
